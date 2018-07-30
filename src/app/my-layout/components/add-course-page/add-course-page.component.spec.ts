85e8cfef54a54e868d4769ab9cd9435c2c7da989
Goshidze <Petrovigor.98@mail.ru>
2018-07-30 23:18:36 +0300

    ~

--- src/app/my-layout/components/add-course-page/add-course-page.component.spec.ts
+++ src/app/my-layout/components/add-course-page/add-course-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AddCoursePageComponent } from './add-course-page.component';
 
 describe('AddCoursePageComponent', () => {
@@ -8,7 +8,8 @@ describe('AddCoursePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddCoursePageComponent ]
+      declarations: [ AddCoursePageComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
